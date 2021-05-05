ff8f3ff5269b23f7998b3a60feae84c83b3d9b2b
yusufdy <60739859+yusufdy@users.noreply.github.com>
2021-05-05 22:14:46 +0700

    Add files via upload

---
+++ sqllite/app/src/main/java/com/example/sqllite/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sqllite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Fragment fragment=new MainFragment();
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.framecontainer,fragment);
+        fragmentTransaction.commit();
+    }
+}
---
+++ sqllite/app/src/main/java/com/example/sqllite/PersonBean.java
@@ -0,0 +1,21 @@
+package com.example.sqllite;
+
+public class PersonBean {
+    String name;
+    int age;
+    public PersonBean() {
+
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
